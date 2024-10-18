38af0eb4381de73dc32b1b9ad4f2df5e5ce534b0
stippusahib <stippusahib2028@cs.sjcetpalai.ac.in>
2024-10-18 15:49:30 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,11 @@
+'''
+Author : S Tippu Sahib
+Date: 18-10-2024
+
+Description: Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+
+x= "hello world"
+y= x[6:12]
+print(y)
+print(y+" Everyone!")
