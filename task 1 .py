ae7c492935d6791c968edc23b2499a68d1e9349a
stippusahib <stippusahib2028@cs.sjcetpalai.ac.in>
2024-10-18 15:15:31 +0530

    Add files via upload

---
+++ task 1 .py
@@ -0,0 +1,13 @@
+'''Write a Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+
+Enter your name: John
+
+Enter your age: 25
+
+Hello, John! You are 25 years old.'''
+
+name = input('enter the name')
+age = input('enter the age')
+print("Hello,", name+"! You are", age,  "years old.")
