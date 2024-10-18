cca3c0e670bd155e1348fe0da079c7d1bfe1afdf
stippusahib <stippusahib2028@cs.sjcetpalai.ac.in>
2024-10-18 15:33:49 +0530

    Update task 1 .py

--- task 1 .py
+++ task 1 .py
@@ -1,13 +1,9 @@
-'''Write a Python program that asks the user to input their name and age.
-After receiving the input, display a message that includes the user's name and age.
-Expected Output:
-
-Enter your name: John
-
-Enter your age: 25
-
-Hello, John! You are 25 years old.'''
-
+'''
+Author : S Tippu Sahib
+Date: 18/10/2024
+Description: Write a Python program that asks the user to input their name and age.
+             After receiving the input, display a message that includes the user's name and age.
+'''
 name = input('enter the name')
 age = input('enter the age')
 print("Hello,", name+"! You are", age,  "years old.")
